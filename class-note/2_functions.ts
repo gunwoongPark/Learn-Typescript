b01fb7ed3a03603aa722fa98b412f88877b05858
parkGunWoong <bung1438@gmail.com>
2021-11-18 01:03:35 +0900

    함수 타입 - 옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -13,4 +13,9 @@ function add(): number {
 function sum(a: number, b: number): number {
   return a + b;
 }
-sum(10, 20, 30, 40);
+sum(10);
+
+// 함수의 옵셔널 파라미터(?)
+function log(a: string, b?: string) {}
+log("hello world");
+log("hello ts", "abc");
