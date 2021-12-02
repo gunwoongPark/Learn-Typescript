fcfdff2a6fb52d58a08a6968f7a5b8ed168fa092
parkGunWoong <bung1438@gmail.com>
2021-12-03 00:18:19 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,18 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 let gunwoong: User = {
   age: 25,
   name: "건웅",
 };
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+const woong = {
+  age: 100,
+  name: "웅",
+};
+getUser(woong);
