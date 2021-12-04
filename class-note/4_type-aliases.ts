8e71d32824a66099c0e14d99364fd01c794c5546
parkGunWoong <bung1438@gmail.com>
2021-12-04 11:09:55 +0900

    유니온 타입의 장점

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -3,22 +3,22 @@
 //   age: number;
 // }
 
-type Person = {
-  name: string;
-  age: number;
-};
+// type Person = {
+//   name: string;
+//   age: number;
+// };
 
-const gunwoong: Person = {
-  name: "건웅",
-  age: 25,
-};
+// const gunwoong: Person = {
+//   name: "건웅",
+//   age: 25,
+// };
 
-type MyString = string;
-const str: MyString = "hello";
+// type MyString = string;
+// const str: MyString = "hello";
 
-type Todo = {
-  id: number;
-  title: string;
-  done: boolean;
-};
-function getTodo(todo: Todo) {}
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+// function getTodo(todo: Todo) {}
