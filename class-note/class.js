48b98d4842e23a8d581a3b8703f8890f6f479196
parkGunWoong <bung1438@gmail.com>
2021-12-20 22:30:36 +0900

    타입스크립트의 클래스 문법

--- class-note/class.js
+++ class-note/class.js
@@ -1,17 +1,17 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-let woong = new Person("건웅", 25);
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// let woong = new Person("건웅", 25);
 
-class Person {
-  // 클래스 로직
-  constructor(name, age) {
-    console.log("생성 되었습니다.");
-    this.name = name;
-    this.age = age;
-  }
-}
+// class Person {
+//   // 클래스 로직
+//   constructor(name, age) {
+//     console.log("생성 되었습니다.");
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
 
-const gunwoong = new Person("건웅", 25);
-console.log(gunwoong);
+// const gunwoong = new Person("건웅", 25);
+// console.log(gunwoong);
