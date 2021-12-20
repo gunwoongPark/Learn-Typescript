670676d570a3fe70fc42c7bcb6ec435c285823d3
parkGunWoong <bung1438@gmail.com>
2021-12-20 22:15:48 +0900

    클래스 소개

---
+++ class-note/class.js
@@ -0,0 +1,11 @@
+class Person {
+  // 클래스 로직
+  constructor(name, age) {
+    console.log("생성 되었습니다.");
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const gunwoong = new Person("건웅", 25);
+console.log(gunwoong);
