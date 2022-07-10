5609a1b23d9fd7a569f29920ace539f0c54b010c
gunwoongPark <bung1438@gmail.com>
2022-07-10 23:14:38 +0900

    제네릭의 타입 제한

--- class-note/8_generic.ts
+++ class-note/8_generic.ts
@@ -58,3 +58,13 @@ interface Dropdown<T> {
   selected: boolean;
 }
 const obj: Dropdown<string> = { value: "abc", selected: false };
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text: T[]): T[] {
+  console.log(text.length);
+  text.forEach(function (text) {
+    console.log(text);
+  });
+  return text;
+}
+logTextLength<string>(["hi", "abc"]);
